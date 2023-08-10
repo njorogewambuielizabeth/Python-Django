bd3a4f70bcab087141920aa1442a7b4a0e1282fb
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-08-10 23:00:05 +0300

    made changes

--- shipping/urls.py
+++ shipping/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('shipments/create/', views.create_shipment, name='create_shipment'),
-    path('shipments/list/', views.shipment_list, name='shipment_list'),
+    path('shipments/list/', views.shipment_list, name='shipment_list_view'),
    path('shipments/edit/<int:id>/',views.edit_shipment, name='edit_shipment_view'),
     path('shipments/<int:id>/',views.shipment_details, name='shipment_details_view'),
  
